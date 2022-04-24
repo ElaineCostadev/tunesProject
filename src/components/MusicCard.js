5bb2b3d1b47b07a69f03730972707579c466cec8
ElaineCosta <elainerscosta@hotmail.com>
2022-04-23 21:53:31 -0300

    requesito 9 ufa

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,25 +1,94 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 export default class MusicCard extends Component {
-  render() {
-    const { trackName, previewUrl } = this.props;
-    return (
-      <div className="music-card">
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      favoriteChecked: false,
 
-        <p trackName={ trackName } />
+    };
+  }
 
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-        </audio>
+  componentDidMount() {
+    const { musicFavorites, music } = this.props;
+    console.log(musicFavorites);
+    const findFavorite = musicFavorites.some((fav) => fav.trackId === music.trackId);
+    if (findFavorite) {
+      console.log('funcionou componentDidMount');
+      this.setState({ favoriteChecked: true });
+    }
+  }
 
-      </div>
-    );
+addMusicFavorites = async ({ target }) => {
+  const { music } = this.props;
+  const { checked } = target;
+  if (checked) {
+    this.setState({
+      loading: true,
+    });
+    await addSong(music);
+    // console.log(api);
+    this.setState({
+      loading: false,
+      favoriteChecked: true,
+    });
+  } else {
+    this.setState({ favoriteChecked: false });
   }
 }
+
+/* else { */
+/*   removed() {
+    removeSong(musicsOrigin).then((removeFavorites) => {
+      this.setState - loadin...true..checked false...
+      console.log(removeFavorites);
+      console.log(params);
+    });
+  } */
+
+render() {
+  const { music } = this.props;
+  // console.log(music);
+  const { loading, favoriteChecked } = this.state;
+
+  if (loading) return <Loading />;
+  return (
+    <div className="music-card">
+
+      <p trackname={ music.trackName } />
+
+      <audio data-testid="audio-component" src={ music.previewUrl } controls>
+        <track kind="captions" />
+        O seu navegador não suporta o elemento
+        <code>audio</code>
+      </audio>
+      <form>
+        <label htmlFor="Favorita">
+          <input
+            checked={ favoriteChecked }
+            onChange={ this.addMusicFavorites }
+            id="Favorita"
+            name="favoriteChecked"
+            data-testid={ `checkbox-music-${music.trackId}` }
+            type="checkbox"
+          />
+        </label>
+      </form>
+
+    </div>
+  );
+}
+}
+
 MusicCard.propTypes = {
-  trackName: PropTypes.string.isRequired,
-  previewUrl: PropTypes.string.isRequired,
+  music: PropTypes.shape({
+    trackId: PropTypes.number.isRequired,
+    trackName: PropTypes.string.isRequired,
+    previewUrl: PropTypes.string.isRequired,
+  }).isRequired,
+  musicFavorites: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Album extends Component {
   constructor() {
@@ -10,32 +11,42 @@ export default class Album extends Component {
     this.state = {
       musics: [],
       musicsOrigin: '',
+      getFavorites: [],
     };
-    // this.apiGetMusics = this.apiGetMusics.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    // ao fazer a async funcionou melhor do que com o then - ajuda do Guilherme Eduardo e Jackson Viana
+    const result = await getFavoriteSongs();
+    this.setState({
+      getFavorites: result,
+    });
+
+    this.pegarMusicas();
+  }
+
+  pegarMusicas = async () => {
     // atraves do match consigo ter o id da Route que fica guardado no params - pego de la o id e pego a API e salvo no state;
     const { match } = this.props;
     const { params } = match;
     // const { musics, musicsOrigin } = this.state; não sei pq, nao precisei desestruturar segundo o Lint, so obedece rs.
-    // console.log(params.id);
-    getMusics(params.id)
-      .then((result) => {
-        // Ajuda mentoria e Jonathan, Samuel e Vinicius.
-        // Pego o 1º item que tem a informacao do artista e album.
-        // const newResult = result[0];
-        // seto os dois states -  um preciso do 1º item  e outro que vou fazer o map
-        this.setState({ musicsOrigin: result[0], musics: result });
-      });
+    const newResult = await getMusics(params.id);
+    this.setState({ musicsOrigin: newResult[0], musics: newResult });
   }
 
+  /*   .then((result) => { */
+  // Ajuda mentoria e Jonathan, Samuel e Vinicius.
+  // Pego o 1º item que tem a informacao do artista e album.
+  // const newResult = result[0];
+  // seto os dois states -  um preciso do 1º item  e outro que vou fazer o map
+  /*    this.setState({ musicsOrigin: result[0], musics: result });
+  }); */
+
   render() {
-    const { musics, musicsOrigin } = this.state;
+    const { musics, musicsOrigin, getFavorites } = this.state;
     // metodo slice retira objetos do array
     // https://www.delftstack.com/pt/howto/javascript/javascript-remove-object-from-array/
     const newMusics = musics.slice(1);
-    // console.log('newMusics', newMusics);
     return (
       <div
         data-testid="page-album"
@@ -61,8 +72,8 @@ export default class Album extends Component {
                   </p>
                 </div>
                 <MusicCard
-                  trackName={ music.trackName }
-                  previewUrl={ music.previewUrl }
+                  music={ music } // passar como paramento para a funcao addSong no filho MusicCard
+                  musicFavorites={ getFavorites }
                 />
               </li>
             ))
@@ -73,6 +84,8 @@ export default class Album extends Component {
   }
 }
 
+// Pesquisa para props de match
+// https://stackoverflow.com/questions/47311310/proptypes-isrequired-on-react-router-4-params-prop
 Album.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
